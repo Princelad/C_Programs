5080ac6459696068d82d0da60796332d99d05006
Princelad <ladprince1112@gmail.com>
2023-12-06 14:51:21 +0530

    2.0

--- PRICS1_7_2.c
+++ PRICS1_7_2.c
@@ -1,32 +1,65 @@
 /*This program is prepared by 23CS037 Prince Kirankumar Lad*/
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     // Creating variables and arrays.
-    int a[10], b[10], c[20] = {0}, L1, L2, swap = 0;
+    int *a, *b, *c, L1, L2, swap = 0;
 
     // Scanning for number of the elements in first array.
     printf("Enter the number of element of the first array:");
     scanf("%d", &L1);
 
+    // Dynamic memory allocation.
+    a = (int *)malloc(L1 * sizeof(int));
+    if (a == NULL)
+    {
+        printf("Error in memory allocation.");
+        exit(0);
+    }
+
     // Scanning the numbers itself.
     for (int i = 0; i < L1; i++)
     {
-        printf("Enter element #%d : ",i+1);
-        scanf("%d", &a[i]);
+        printf("Enter element #%d : ", i + 1);
+        if (scanf("%d", &a[i]) != 1)
+        {
+            printf("Error in scanning.Enter a integer value.");
+            exit(1);
+        }
     }
 
     // Scanning the number of the elements in second array.
     printf("Enter the number of elements of the second array:");
     scanf("%d", &L2);
 
+    // Dynamic memory allocation.
+    b = (int *)malloc(L2 * sizeof(int));
+    if (b == NULL)
+    {
+        printf("Error in memory allocation.");
+        exit(0);
+    }
+
     // Scanning numbers.
     for (int i = 0; i < L2; i++)
     {
-        printf("Enter element #%d : ",i+1);
-        scanf("%d", &b[i]);
+        printf("Enter element #%d : ", i + 1);
+        if (scanf("%d", &b[i]) != 1)
+        {
+            printf("Error in scanning.Enter a integer value.");
+            exit(1);
+        }
+    }
+
+    // Dynamic memory allocation.
+    c = (int *)malloc((L1 + L2) * sizeof(int));
+    if (c == NULL)
+    {
+        printf("Error in memory allocation.");
+        exit(0);
     }
 
     // Merging both arrays.
@@ -63,6 +96,11 @@ int main()
     {
         printf("%d ", c[i]);
     }
+
+    // Freeing the space.
+    free(a);
+    free(b);
+    free(c);
 
     printf("\n\n23CS037_Prince\n");
     return 0;
